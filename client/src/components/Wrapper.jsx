f1cfb55b27c14fddf5177cf49bf333e764acd3cd
darshit <darshit.hs@somaiya.edu>
2024-01-25 23:39:36 +0530

    wrapper

--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -1,41 +1,36 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useLocation } from 'react-router-dom';
-
+import React, { useEffect, useState, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLocation } from "react-router-dom";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Wrapper = ({ children }) => {
-
   const scrollManager = useRef(null);
   const location = useLocation();
+  console.log(location.pathname);
 
   useEffect(() => {
-
     const animationTimeout = setTimeout(() => {
       const appearElements = document.querySelectorAll(".appear");
 
       appearElements.forEach((element) => {
-        gsap.from(
-          element,
-          {
-            x: 60,
-          
-            y: 1,
-            duration: 1.5,
-            opacity: 0,
-            scrollTrigger: {
-              trigger: element,
-              start: "top bottom",
-              end: "bottom top",
-              scroller: scrollManager.current,
-              toggleActions: "restart play restart none",
-              // scrub: true,
-              // markers: true,
-            }
-          }
-        );
+        gsap.from(element, {
+          x: 60,
+
+          y: 1,
+          duration: 1.5,
+          opacity: 0,
+          scrollTrigger: {
+            trigger: element,
+            start: "top bottom",
+            end: "bottom top",
+            scroller: scrollManager.current,
+            toggleActions: "restart play restart none",
+            // scrub: true,
+            // markers: true,
+          },
+        });
       });
     }, 100); // Adjust the delay as needed
 
@@ -48,13 +43,16 @@ const Wrapper = ({ children }) => {
     <div
       ref={scrollManager}
       style={{
-        width: '100%',
-        minHeight: 'calc(100vh - 82px)',
-        overflowY: 'auto',
-        backgroundColor: '#eee',
-        position: 'relative',
-        padding:'10px',
-        maxHeight: 'calc(100vh - 82px)',
+        width: "100%",
+        minHeight:
+          location.pathname === "/login" || location.pathname === "/new-profile"
+            ? "100vh"
+            : "calc(100vh - 82px)",
+        overflowY: "auto",
+        backgroundColor: "#eee",
+        position: "relative",
+        padding: "10px",
+        maxHeight: "calc(100vh - 82px)",
       }}
     >
       {children}

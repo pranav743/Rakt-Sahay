d35d8d6adcd6688bcc6c4f6ed42ab9849965886a
Pranav Patil <pranav07@somaiya.edu>
2024-01-25 15:57:24 +0530

    Schemas Created

--- server/models/hospital.js
+++ server/models/hospital.js
@@ -1,40 +1,92 @@
 const mongoose = require('mongoose');
 
+const bloodGroupSchema = new mongoose.Schema({
+    bloodType: {
+        type: String,
+        enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+        required: true,
+    },
+    stored: {
+        type: Number,
+        default: 0,
+    },
+    required: {
+        type: Number,
+        default: 0,
+    },
+});
 
 const hospitalSchema = new mongoose.Schema({
-
     name: {
         type: String,
-        required: [true, "Mentor Name is required"]
+        required: [true, "Hospital name is required"],
     },
     email: {
         type: String,
         required: [true, "E-mail is mandatory"],
-        validate: {
-            validator: validateEmail,
-            message: "Email must be from @somaiya.edu domain"
-        }
     },
     location: {
         latitude: {
             type: String,
-            required: true
+            required: true,
         },
         longitude: {
             type: String,
-            required: true
+            required: true,
         },
     },
     address: {
         type: String,
-        required: true
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    contactNo: {
+        type: String,
+        required: true,
+    },
+    hospitalId: {
+        type: String,
+        required: true,
+        unique: true,
     },
     isActive: {
         type: Boolean,
         required: false,
-        default: true
+        default: true,
     },
-
+    currentBlood: [bloodGroupSchema],
+    bloodRequirements: [bloodGroupSchema],
+    completeDonations: [
+        {
+            donor: {
+                type: String,
+                required: true
+            },
+            receiver: {
+                type: String,
+                required: true
+            },
+            time: {
+                type: Date,
+                default: Date.now
+            },
+            bloodGroup: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: String,
+                required: true
+            }
+        },
+    ],
 });
 
 const Hospital = mongoose.model('Hospital', hospitalSchema);
---
+++ server/models/users.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "User name is required"],
+    },
+    profilePicture: {
+        type: String,
+        required: false
+    },
+    sub_id: {
+        type: String,
+        required: false
+    },
+    contact_no: {
+        type: String,
+        required: [true, "Contact no is Required"]
+    },
+    email: {
+        type: String,
+        required: [true, "E-mail is mandatory"],
+        unique: true,
+    },
+    healthIssues: [
+        {type: String}
+    ],
+    contactNo: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    bloodType: {
+        type: String,
+        enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+        required: true,
+    },
+    isDonor: {
+        type: Boolean,
+        default: false,
+    },
+    donations: [{
+        date: {
+            type: Date
+        },
+        location: {
+            type: String,
+            required: false
+        }
+    }],
+    isRecipient: {
+        type: Boolean,
+        default: false,
+    },
+    receivingAt: {
+        hospital: {
+            type: String,
+            required: [true, "Hospital email is required"]
+        },
+        hospitalName: {
+            type: String,
+            required: [true, "Hospital Name is required"]
+        }
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

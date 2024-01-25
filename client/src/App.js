36cf9b2d33cc8e5949352481250813b59cbbd3d0
darshit <darshit.hs@somaiya.edu>
2024-01-25 15:19:09 +0530

    file_structure_and_routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
-import { MemoryRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/login/Login';
+import React from "react";
+import "./App.css";
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/login/Login";
+import BottomNav from "./components/bottomNav";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/login" element={<Login />} />
+          <Route path="/" exact component={SplashScreen} />
+          <Route path="/login-screen" exact component={LoginScreen} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/sign-up" exact component={SignUpPage} />
+          <Route component={BottomNav}>
+            <Route path="/home" exact component={HomePage} />
+            <Route path="/find-donor" exact component={FindDonorPage} />
+            <Route path="/request-donor" exact component={RequestDonorPage} />
+            <Route path="/profile" exact component={ProfilePage} />
+          </Route>
         </Routes>
       </Router>
     </div>

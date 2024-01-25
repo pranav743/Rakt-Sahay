ee3cc457bf0b4d3fd041e40b0e38115be9ffa327
Pranav Patil <pranav07@somaiya.edu>
2024-01-25 16:38:24 +0530

    added Wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,24 @@ import {
 } from "react-router-dom";
 import BottomNav from "./components/bottomNav";
 import Splash from "./screens/splash";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" exact element={<Splash />} />
-          {/* <Route path="/login-screen" exact component={LoginScreen} />
+        <Wrapper>
+          <Routes>
+            <Route path="/" exact element={<Splash />} />
+            {/* <Route path="/login-screen" exact component={LoginScreen} />
           <Route path="/login" exact component={LoginPage} />
           <Route path="/sign-up" exact component={SignUpPage} />
           <Route path="/home" exact component={HomePage} />
           <Route path="/find-donor" exact component={FindDonorPage} />
           <Route path="/request-donor" exact component={RequestDonorPage} />
           <Route path="/profile" exact component={ProfilePage} /> */}
-        </Routes>
+          </Routes>
+        </Wrapper>
       </Router>
     </div>
   );

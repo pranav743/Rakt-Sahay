c036292d88a069d6383276e47131a2a1910935ad
darshit <darshit.hs@somaiya.edu>
2024-01-25 16:01:24 +0530

    icons

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,22 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import Login from "./pages/login/Login";
 import BottomNav from "./components/bottomNav";
+import Splash from "./screens/splash";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" exact component={SplashScreen} />
-          <Route path="/login-screen" exact component={LoginScreen} />
+          <Route path="/" exact element={<Splash />} />
+          {/* <Route path="/login-screen" exact component={LoginScreen} />
           <Route path="/login" exact component={LoginPage} />
           <Route path="/sign-up" exact component={SignUpPage} />
-          <Route component={BottomNav}>
-            <Route path="/home" exact component={HomePage} />
-            <Route path="/find-donor" exact component={FindDonorPage} />
-            <Route path="/request-donor" exact component={RequestDonorPage} />
-            <Route path="/profile" exact component={ProfilePage} />
-          </Route>
+          <Route path="/home" exact component={HomePage} />
+          <Route path="/find-donor" exact component={FindDonorPage} />
+          <Route path="/request-donor" exact component={RequestDonorPage} />
+          <Route path="/profile" exact component={ProfilePage} /> */}
         </Routes>
       </Router>
     </div>
--- client/src/screens/splash.jsx
+++ client/src/screens/splash.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import BottomNav from "../components/bottomNav";
 
 const Splash = () => {
-  return <div>Splash</div>;
+  return <BottomNav />;
 };
 
 export default Splash;

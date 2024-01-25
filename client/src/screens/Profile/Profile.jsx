bf88709550adffe52942aa961e30e2dd47415ec0
darshit <darshit.hs@somaiya.edu>
2024-01-25 23:14:55 +0530

    front_back

--- client/src/screens/Profile/Profile.jsx
+++ client/src/screens/Profile/Profile.jsx
@@ -10,24 +10,54 @@ import {
 
 const Profile = () => {
   return (
-    <Accordion>
-      <AccordionItem>
-        <h2>
-          <AccordionButton _expanded={{ bg: "tomato", color: "white" }}>
-            <Box as="span" flex="1" textAlign="left">
-              Click me to see a different style
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </h2>
-        <AccordionPanel>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </AccordionPanel>
-      </AccordionItem>
-    </Accordion>
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold text-center my-4">Profile</h1>
+      <div className="pic size-40 rounded-full border-2 border-solid border-[#EA3A60] my-3"></div>
+      <div className="accordian w-full max-w-sm mt-4 text-xl">
+        <Accordion allowToggle>
+          <AccordionItem>
+            <h2>
+              <AccordionButton
+                _expanded={{ bg: "#EA3A60", color: "white" }}
+                fontSize="2xl" // You can adjust the size as needed (lg stands for large)
+                padding={4}
+              >
+                <Box as="span" flex="1" textAlign="left">
+                  Personal Information
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton
+                _expanded={{ bg: "#EA3A60", color: "white" }}
+                fontSize="2xl" // You can adjust the size as needed (lg stands for large)
+                padding={4}
+              >
+                <Box as="span" flex="1" textAlign="left">
+                  Donation History
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
   );
 };
 

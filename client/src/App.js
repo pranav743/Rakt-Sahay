570c805e315b2bdf91395a234eb90913a3d56d16
Pranav Patil <pranav07@somaiya.edu>
2024-01-26 20:01:57 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -48,7 +48,7 @@ function App() {
           <Route
             path="/redirection/:accessToken"
             element={<RedirectionPage />}
-          /> */}
+          /> 
         </Routes>
       </Wrapper>
       <BottomNav />

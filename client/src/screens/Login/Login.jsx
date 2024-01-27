959120a9871d95acdb1495897b8b45f7364d44f8
Pranav Patil <pranav07@somaiya.edu>
2024-01-27 10:58:07 +0530

    More login scrren changes

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -3,9 +3,12 @@ import GoogleButton from "react-google-button";
 import { Center, Square, Circle, Box, Divider } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { url } from "../../Global/URL";
+import showToast from "../../Global/Toast";
+import { useToast } from "@chakra-ui/react";
 
 const Login = () => {
   const navigate = useNavigate();
+  const toast = useToast();
   const [isChecked, setIsChecked] = useState(false);
 
   const handleCheckboxChange = () => {
@@ -17,14 +20,14 @@ const Login = () => {
       // Perform the login action
       window.location.href = url + "/login";
     } else {
-      // Display an error message or handle accordingly
+      showToast(toast, "", "info", "Please Accept Terms and Conditions !");
       console.log("Please accept the terms and conditions.");
     }
   };
 
   return (
     <div className="flex-col items-center justify-center mx-auto">
-      <div className="mt-10 p-6 bg-white rounded-md shadow-lg w-80 sm:w-96 mx-auto text-center">
+      <div className="mt-10 p-6 bg-white rounded-md shadow-lg w-80 sm:w-96 mx-auto text-center" style={{marginTop: '18vh'}}>
         <h2 className="text-4xl font-extrabold text-gray-800">Welcome to <span style={{ color: 'red' }}>Rakt</span> Sahay!</h2>
         <Divider mt={10}/>
         <p className="text-lg text-gray-600 mt-2">
